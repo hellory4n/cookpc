b0c5038e7b628b8cd0d1ecac654d67ff0bbc7111
hellory4n <hellory4n@gmail.com>
2022-09-10 19:21:18 -0500

    hi momm

---
+++ Assets/Game.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Game : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log("Hi mom");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
