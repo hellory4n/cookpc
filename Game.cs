7092261903d491f30a95abb4a0b948f0e554ef81
hellory4n <hellory4n@gmail.com>
2022-09-13 22:23:14 -0500

    boom

--- Game.cs
+++ Game.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using CookPC.VM;
 
 public class Game : Node2D {
@@ -7,17 +8,21 @@ public class Game : Node2D {
 	private int loopCounter = 0;
 	// TODO: Get this from the cookpc filesystem
 	private string bootScript = 
-@"testing h
-tests h
-testing h
-tests h
-testing h
-tests h
-testing h
-tests h";
+@"mcalloc 0
+mcalloc 0
+mcset 0
+mvdef c 0
+add $c 1 c
+debug $c
+equal $c 10 urmom
+not $urmom urmom
+ifjump 4 $urmom";
 	private int maxInstruction;
 	private int currentInstruction = 0;
 	private string instruction;
+	private List<Dictionary<string, dynamic>> memory = new List<Dictionary<string, dynamic>>();
+	private int currentChunk;
+	private int variableCount = 0;
 
 	public override void _Ready() {
 		maxInstruction = bootScript.Split("\n").Length;
@@ -26,18 +31,34 @@ tests h";
 	}
 
 	public override void _Process(float delta) {
-		while (loopCounter < 500) {
-				var jsssjjsjshshsj = Lexer.Tokenize(instruction);
-				InstructionRunner.Run(jsssjjsjshshsj);
+		// Run stuff :)
+		// TODO: Make it blazingly fast
+		while (loopCounter < 60) {
+			var jsssjjsjshshsj = Lexer.Tokenize(instruction, memory, currentChunk);
+			/*foreach (var item in jsssjjsjshshsj) {
+				System.Console.WriteLine(item);
+			}*/
+			(memory, currentChunk, variableCount, currentInstruction) = InstructionRunner.Run(jsssjjsjshshsj, memory, currentChunk, variableCount, currentInstruction);
 
-				currentInstruction++;
-				if (currentInstruction == maxInstruction)
-					currentInstruction = 0;
+			currentInstruction++;
+			if (currentInstruction == maxInstruction)
+				currentInstruction = 0;
 
-				instruction = bootScript.Split("\n")[currentInstruction];
+			instruction = bootScript.Split("\n")[currentInstruction];
 
-				loopCounter++;
-			}
-			loopCounter = 0;
+			loopCounter++;
+		}
+
+		/*foreach (KeyValuePair<string, dynamic> m in memory[currentChunk]) {
+			System.Console.WriteLine(m.Key + ": " + m.Value);
+		}*/
+
+		loopCounter = 0;
+
+		// We don't want people to create 694201337 variables, that's illegal
+		// TODO: Get this from somewhere
+		// TODO: Don't make the limit this stupid
+		if (variableCount > 9) 
+			GetTree().Quit();
 	}
 }
--- VM/InstructionRunner.cs
+++ VM/InstructionRunner.cs
@@ -1,22 +1,171 @@
+
 using System.Collections.Generic;
 using System.Linq;
+using System;
 using Godot;
 
 namespace CookPC.VM {
     class InstructionRunner {
-        public static void Run(List<string> instruction) {
+        public static (List<Dictionary<string, dynamic>>, int, int, int) Run(List<string> instruction, List<Dictionary<string, dynamic>> memory, int currentChunk, int variableCount, int currentInstruction) {
             var method = instruction[0];
             var args = instruction.Skip(1).ToList();
 
             switch (method) {
-                case "testing":
-                    GD.Print("oh ma gawd");
+                #region Memory instructions
+
+                case "mcalloc":
+                    // Mega complicated logic.
+                    memory.Add(new Dictionary<string, dynamic>());
                     break;
 
-                case "tests":
-                    GD.Print("the testing tests work");
+                case "mcset":
+                    // int arg0: chunk id
+                    var _ = int.TryParse(args[0], out int chunkID);
+                    currentChunk = chunkID;
                     break;
+                
+                case "mvdef":
+                    // str arg0: variable name
+                    // any arg1: value
+
+                    if (!memory[currentChunk].ContainsKey(args[0]))
+                        variableCount++;
+                    memory[currentChunk][args[0]] = (dynamic)args[1];
+                    break;
+                
+                case "mvfree":
+                    // str arg0: variable name
+
+                    memory[currentChunk].Remove(args[0]);
+                    variableCount--;
+                    break;
+                
+                case "mcamount":
+                    // str arg0: the variable the result will be saved to
+
+                    memory[currentChunk][args[0]] = memory.Count;
+                    break;
+                
+                case "mvamount":
+                    // int arg0: chunk id
+                    // str arg1: the variable the result will be saved to
+                    var __ = int.TryParse(args[0], out int cHashtag);
+                    if (cHashtag == -1)
+                        memory[currentChunk][args[1]] = variableCount;
+                    else
+                        memory[currentChunk][args[1]] = memory[cHashtag].Count;
+
+                    break;
+
+                #endregion
+
+                #region Misc
+
+                case "debug":
+                    GD.Print(args[0]);
+                    break;
+                
+                case "strcomb":
+                    memory[currentChunk][args[2]] = args[0].ToString() + args[1].ToString();
+                    break;
+
+                #endregion
+
+                #region Boolean stuff
+
+                case "not":
+                    var ___ = bool.TryParse(args[0], out bool one);
+                    memory[currentChunk][args[1]] = !one;
+                    break;
+                
+                case "and":
+                    var ____ = bool.TryParse(args[0], out bool two);
+                    var _____ = bool.TryParse(args[1], out bool three);
+                    memory[currentChunk][args[2]] = two && three;
+                    break;
+                
+                case "or":
+                    var ______ = bool.TryParse(args[0], out bool four);
+                    var _______ = bool.TryParse(args[1], out bool five);
+                    memory[currentChunk][args[2]] = four || five;
+                    break;
+                
+                case "equal":
+                    memory[currentChunk][args[2]] = args[0].ToString() == args[1].ToString();
+                    break;
+                
+                case "less":
+                    var ________ = double.TryParse(args[0], out double six);
+                    var _________ = double.TryParse(args[1], out double seven);
+                    memory[currentChunk][args[2]] = six < seven;
+                    break;
+                
+                case "greater":
+                    var __________ = double.TryParse(args[0], out double eight);
+                    var ___________ = double.TryParse(args[1], out double nine);
+                    memory[currentChunk][args[2]] = eight > nine;
+                    break;
+
+                #endregion
+
+                #region Me- uhh i mean math
+
+                case "add":
+                    var ____________ = double.TryParse(args[0], out double ten);
+                    var _____________ = double.TryParse(args[1], out double eleven);
+                    memory[currentChunk][args[2]] = ten + eleven;
+                    break;
+                
+                case "subtract":
+                    var ______________ = double.TryParse(args[0], out double twelve);
+                    var _______________ = double.TryParse(args[1], out double thirteen);
+                    memory[currentChunk][args[2]] = twelve - thirteen;
+                    break;
+                
+                case "multiply":
+                    var ________________ = double.TryParse(args[0], out double fourteen);
+                    var _________________ = double.TryParse(args[1], out double fifteen);
+                    memory[currentChunk][args[2]] = fourteen * fifteen;
+                    break;
+                
+                case "divide":
+                    var __________________ = double.TryParse(args[0], out double sixteen);
+                    var ___________________ = double.TryParse(args[1], out double seventeen);
+                    memory[currentChunk][args[2]] = sixteen / seventeen;
+                    break;
+                
+                case "modulo":
+                    var ____________________ = double.TryParse(args[0], out double eighteen);
+                    var _____________________ = double.TryParse(args[1], out double nineteen);
+                    memory[currentChunk][args[2]] = eighteen % nineteen;
+                    break;
+                
+                case "power":
+                    var ______________________ = double.TryParse(args[0], out double twenty);
+                    var _______________________ = double.TryParse(args[1], out double twentyone);
+                    memory[currentChunk][args[2]] = Math.Pow(twenty, twentyone);
+                    break;
+
+                #endregion
+
+                #region Control
+
+                case "jump":
+                    var ________________________ = int.TryParse(args[0], out int twentytwo);
+                    currentInstruction = twentytwo-1; // the interpreter will increment the currentInstruction counter
+                    break;
+                
+                case "ifjump":
+                    var _________________________ = int.TryParse(args[0], out int twentythree);
+                    var __________________________ = bool.TryParse(args[1], out bool twentyfour);
+                    if (twentyfour)
+                        currentInstruction = twentythree-1; // the interpreter will increment the currentInstruction counter
+                    break;
+
+                #endregion
             }
+
+            return (memory, currentChunk, variableCount, currentInstruction);
         }
     }
 }
